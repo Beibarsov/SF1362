56b7d2bf3c50b68fc68c5b451e0c0129283fa805
Beibarsov <BarsStudio@gmail.com>
2022-04-03 13:29:54 +0800

    first commit

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+using System.Collections.Generic;
+
+Dictionary<string, int> favoriteWords = new Dictionary<string, int>();
+
+var text = File.ReadAllText(@"Text1.txt");
+string textNoPunctuation = new string(text.Where(x => !char.IsPunctuation(x)).ToArray());
+char[] decim = new char[3]{' ', '\n', '\t'};
+string[] words = textNoPunctuation.Split(decim, StringSplitOptions.RemoveEmptyEntries);
+
+
+
+foreach (string word in words)
+{   
+    var newword = word.ToLower().TrimEnd();
+    if (favoriteWords.ContainsKey(newword))
+    {
+        favoriteWords[newword] += 1;
+    }
+    else
+    {
+        favoriteWords.Add(newword, 1);
+    }
+
+}
+
+var ordered = favoriteWords.OrderByDescending(x => x.Value).Take(10);
+
+
+
+foreach (var word in ordered){
+    Console.WriteLine(word.Key + " Количество повторений: " +  word.Value);
+}
